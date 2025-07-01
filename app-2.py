9c872eedd06d828cdee4043bef9ecbbd3e90ccee
gurkan-maker <gurkanduran2@gmail.com>
2025-07-01 15:52:56 +0300

    Update app-2.py

--- app-2.py
+++ app-2.py
@@ -389,7 +389,7 @@ def reynolds_number(flow_m3h: float, d_m: float, visc_cst: float) -> float:
         return 1e6
     N4 = CONSTANTS["N4"]["mm"]
     d_mm = d_m * 1000
-    return N4 * flow_m3h / (visc_cst * d_mm)
+    return flow_m3h / (visc_cst * d_mm)
 
 def viscosity_correction(rev: float) -> float:
     if rev >= 40000:
