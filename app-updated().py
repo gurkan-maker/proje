f55c88a603ba785dfaac906bdbd595c062b746f2
gurkan-maker <gurkanduran2@gmail.com>
2025-07-16 11:14:02 +0300

    Update app-updated().py

--- app-updated().py
+++ app-updated().py
@@ -1030,7 +1030,7 @@ def get_simulation_image(valve_name):
         "2\" E33": "https://raw.githubusercontent.com/gurkan-maker/proje/Final3/2.0e33.png",
         "4\" E33": "https://raw.githubusercontent.com/gurkan-maker/proje/Final3/4.0e33.png",
         "8\" E33": "https://raw.githubusercontent.com/gurkan-maker/proje/Final3/8e33.png",
-        "8\" E43": "https://raw.githubusercontent.com/gurkan-maker/proje/Final3/8e43.png",
+        "8.0\" E43": "https://raw.githubusercontent.com/gurkan-maker/proje/refs/heads/Final3/8e43.png",
         "12\" E33": "https://raw.githubusercontent.com/gurkan-maker/proje/Final3/12.0e33.png",
         "16\" E33": "https://raw.githubusercontent.com/gurkan-maker/proje/Final3/16.0e33.png",
         "20\" E33": "https://raw.githubusercontent.com/gurkan-maker/proje/Final3/20.0e33.png",
